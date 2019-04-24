636f006c619bf91194d989cfa50e97273fd28150
Le Nhu <nhu.le@gameloft.com>
2019-04-24 13:53:53 +0700

    + Add abstract class model.cs

---
+++ Robo/src/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Robo.src
+{
+    abstract class Model
+    {
+        protected bool                  m_isBusy;
+        List<Cell>                      m_pathPlanning;
+        public abstract void OnUpdate();
+        public abstract void OnInit();
+
+    }
+}
